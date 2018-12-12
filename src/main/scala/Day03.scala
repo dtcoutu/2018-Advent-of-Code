3aaa3826428f687e82f16d31aaff282cbd49bdbd
David Coutu <dcoutu@inntopia.com>
2018-12-12 16:13:01 -0500

    Completed day 3 part 1 - though I suspect this could be more efficient.

--- src/main/scala/Day03.scala
+++ src/main/scala/Day03.scala
@@ -2,23 +2,71 @@ import javax.print.attribute.standard.MediaSize.Other
 
 object Day03 {
   case class Point(x: Int, y: Int)
-  case class Rectangle(width: Int, height: Int)
-  case class Claim(id: Int, point: Point, rectangle: Rectangle) {
-    val l: Point = point
-    val r: Point = Point(point.x + rectangle.width, point.y + rectangle.height)
-
-    def overlaps(other: Claim): Boolean = {
-      println("L: " + l + "; R: " + r)
-      println("Other L: " + other.l + "; R: " + other.r)
-      if (l.x > other.r.x || other.l.x > r.x) false
-      else if (l.y < other.r.y || other.l.y < r.y) false
-      else true
+  case class Rectangle(left: Int, right: Int, top: Int, bottom: Int)
+  case class Claim(id: String, rectangle: Rectangle) {
+    def grid: List[List[String]] = {
+      (0 until rectangle.bottom).toList.map { i =>
+        if (i < rectangle.top) List.fill(rectangle.right)(".")
+        else List.fill(rectangle.left)(".") ++ List.fill(rectangle.right-rectangle.left)(id)
+      }
     }
+    def overlaps(other: Claim): Boolean = {
+      if (rectangle.left < other.rectangle.right
+        && rectangle.right > other.rectangle.left
+        && rectangle.top < other.rectangle.bottom
+        && rectangle.bottom > other.rectangle.top)
+        true
+      else false
+    }
+  }
+
+  def mergeClaims(claims: List[Claim]): List[List[String]] = {
+    def go(remainingClaims: List[Claim], fabricGrid: List[List[String]]): List[List[String]] =
+      remainingClaims match {
+        case claim :: tail => go(tail, mergeClaim(fabricGrid, claim))
+        case Nil => fabricGrid
+      }
+
+    go(claims, List())
+  }
+
+  def mergeClaim(fabricGrid: List[List[String]], claim: Claim): List[List[String]] = {
+    (fabricGrid.zipAll(claim.grid, List(), List())).map{ mergeRow(_) }
+  }
+
+  def mergeRow(row: (List[String], List[String])): List[String] = {
+    (row._1.zipAll(row._2, ".", ".")).map{ mergeCell(_) }
+  }
+
+  def mergeCell(cell: (String, String)): String = cell match {
+    case (".", ".") => "."
+    case (_, "X") => "X"
+    case ("X", _) => "X"
+    case (".", id) => id
+    case (id, ".") => id
+    case (_, _) => "X"
+  }
+
+  def duplicateClaimCount(fabricGrid: List[List[String]]): Int = {
+    fabricGrid.map { row =>
+      row.count(_ == "X")
+    }.foldLeft(0)(_ + _)
+  }
+
+  def generateClaims(input: List[String]): List[Claim] = {
+    input.map(parseClaim)
   }
 
   def parseClaim(input: String): Claim = {
     val pattern = "#([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)".r
-    val pattern(id, x, y, h, w) = input
-    Claim(id.toInt, Point(x.toInt, y.toInt), Rectangle(h.toInt, w.toInt))
+    val pattern(id, l, t, w, h) = input
+    Claim(id,
+      Rectangle(l.toInt, (l.toInt + w.toInt),
+                t.toInt, (t.toInt + h.toInt)))
+  }
+
+  def main(args: Array[String]): Unit = {
+    val claims = generateClaims(InputReader.readInput("Day03Input.txt"))
+    println("Overlapping Claims: " + duplicateClaimCount(mergeClaims(claims)))
   }
 }
--- src/test/scala/Day03Spec.scala
+++ src/test/scala/Day03Spec.scala
@@ -3,19 +3,78 @@ import org.scalatest._
 class Day03Spec extends FlatSpec with Matchers {
   "Parse claim" should "convert line of input into a claim" in {
     assert(Day03.parseClaim("#1 @ 1,3: 4x4") ==
-      Day03.Claim(1, Day03.Point(1,3), Day03.Rectangle(4,4)))
+      Day03.Claim("1", Day03.Rectangle(1, 5, 3, 7)))
 
     assert(Day03.parseClaim("#2 @ 3,1: 4x4") ==
-      Day03.Claim(2, Day03.Point(3,1), Day03.Rectangle(4,4)))
+      Day03.Claim("2", Day03.Rectangle(3, 7, 1, 5)))
   }
 
-  "Claim overlaps" should "indicate if two claims overlap each other" in {
-    val baseClaim = Day03.Claim(1, Day03.Point(1,3), Day03.Rectangle(4,4))
+  "Claim grid" should "return the fabric grid for the claim" in {
+    val actual1 = Day03.Claim("1", Day03.Rectangle(1,5,3,7)).grid
 
-    val testClaim1 = Day03.Claim(2, Day03.Point(3,2), Day03.Rectangle(4,4))
-    assert(baseClaim.overlaps(testClaim1))
+    val expected1 = List(
+      List(".",".",".",".","."),
+      List(".",".",".",".","."),
+      List(".",".",".",".","."),
+      List(".","1","1","1","1"),
+      List(".","1","1","1","1"),
+      List(".","1","1","1","1"),
+      List(".","1","1","1","1")
+    )
 
-    val testClaim2 = Day03.Claim(3, Day03.Point(5,7), Day03.Rectangle(1,1))
-    assert(!baseClaim.overlaps(testClaim2))
+    assert(actual1 == expected1)
+
+    var actual2 = Day03.Claim("2", Day03.Rectangle(3,7,1,5)).grid
+    var expected2 = List(
+      List(".",".",".",".",".",".","."),
+      List(".",".",".","2","2","2","2"),
+      List(".",".",".","2","2","2","2"),
+      List(".",".",".","2","2","2","2"),
+      List(".",".",".","2","2","2","2")
+    )
+
+    assert(actual2 == expected2)
+  }
+
+  "Merge row" should "combine rows to a single one" in {
+    assert(Day03.mergeRow(List(), List(".",".",".",".","."))
+      == List(".",".",".",".","."))
+    assert(Day03.mergeRow(List(".","1","X","."), List("2","2","2","."))
+      == List("2","X","X","."))
+    assert(Day03.mergeRow(List(".","X"), List(".","1","1"))
+      == List(".","X","1"))
+  }
+
+  "Merge claims" should "combine grids to a single one" in {
+    val claim1 = Day03.Claim("1", Day03.Rectangle(1,5,3,7))
+    val claim2 = Day03.Claim("2", Day03.Rectangle(3,7,1,5))
+
+    val expected = List(
+      List(".",".",".",".",".",".","."),
+      List(".",".",".","2","2","2","2"),
+      List(".",".",".","2","2","2","2"),
+      List(".","1","1","X","X","2","2"),
+      List(".","1","1","X","X","2","2"),
+      List(".","1","1","1","1"),
+      List(".","1","1","1","1")
+    )
+
+    val actual = Day03.mergeClaims(List(claim1, claim2))
+
+    assert(actual == expected)
+  }
+
+  "Count duplicate claims" should "return the number of squares with multiple claims" in {
+    val fabric = List(
+      List(".",".",".",".",".",".","."),
+      List(".",".",".","2","2","2","2"),
+      List(".",".",".","2","2","2","2"),
+      List(".","1","1","X","X","2","2"),
+      List(".","1","1","X","X","2","2"),
+      List(".","1","1","1","1"),
+      List(".","1","1","1","1")
+    )
+
+    assert(Day03.duplicateClaimCount(fabric) == 4)
   }
 }
